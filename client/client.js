e5401587516543176f3864b844f10d1196d4f9ee
Max Fr <fruehaufmaximilian@gmail.com>
2019-05-25 22:48:48 +0200

    add client

--- client/client.js
+++
@@ -1,6 +0,0 @@
-console.log('client started!!!')
-
-const host = '192.168.0.108';
-const port = 6969;
-
-const ws = new WebSocket('ws://localhost/game');
--- server/server.js
+++ server/server.js
@@ -14,14 +14,29 @@ server.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
 
-let players = [];
+let gameState = {
+  width: 100,
+  height: 100,
+  players: {
+    'Oli': {
+      parts: [[0, 0], [1, 0]],
+    }
+  },
+  food: [[1, 1]]
+}
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send(`echo: ${message}`);
+    if (message.type == 'move') {
+      let p = gameState.players[message.name];
+      p.parts.push()
+    }
+    
+    
+    ws.send(JSON.stringify(gameState));
   });
 
   ws.send('something');
